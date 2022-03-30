e272ff4f2785434407e1afacd6d0a3940e131212
Hira Balcı <93313737+HiraBalci@users.noreply.github.com>
2022-03-30 17:54:59 +0300

    Homework-4

---
+++ First.App/First.App.Core/Abstract/ICompanyService.cs
@@ -0,0 +1,15 @@
+﻿using First.App.Business.DTOs;
+using First.App.Domain.Entities;
+using System.Collections.Generic;
+
+namespace First.App.Business.Abstract
+{
+    public interface ICompanyService
+    {
+        List<Company> GetAllCompany();//niye liste yaptım?Çünkü birden fazla katmana gelebilir.
+        void AddCompany(Company company);
+        void UpdateCompany(Company company);
+        void DeleteCompany(Company company);
+         
+    }
+}
---
+++ First.App/First.App.DataAccess.EntityFramework/Configurations/CompanyConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using First.App.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace First.App.DataAccess.EntityFramework.Configurations
+{
+    public class CompanyConfiguration : IEntityTypeConfiguration<Company>// <> içerisne tablo adı yazıyorsun.
+    {
+        public void Configure(EntityTypeBuilder<Company> builder)// companyi implent etti
+        {
+            builder.ToTable("Companies");// nokta diyip haskey şeklinde de devam edebilirdin.
+            builder.HasKey(x=> x.Id);//primary keyini belirtmiş oluyorsun.
+            builder.Property(x => x.Name).IsRequired();
+        }
+    }
+}
