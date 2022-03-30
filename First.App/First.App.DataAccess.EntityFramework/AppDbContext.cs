3cfc4c67cb2237e392b4e6e823c1dc21d5e2fb43
Hira Balcı <93313737+HiraBalci@users.noreply.github.com>
2022-03-30 12:14:26 +0300

    deneme

---
+++ First.App/First.App.DataAccess.EntityFramework/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using First.App.DataAccess.EntityFramework.Configurations;
+using First.App.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace First.App.DataAccess.EntityFramework
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Company> Companies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new CompanyConfiguration());//buda tablo oluşturur.has key falan da yazabilirsin ama tablomun
+            //configuration class olucak dedim.
+        }
+
+    }
+}
