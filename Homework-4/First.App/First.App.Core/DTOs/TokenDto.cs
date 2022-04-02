38d2eb5b02ad52ae990002f983d53eaea11ab226
Hira Balcı <93313737+HiraBalci@users.noreply.github.com>
2022-04-02 10:01:31 +0300

    delete

--- Homework-4/First.App/First.App.Core/DTOs/TokenDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace First.App.Business.DTOs
-{
-    public class TokenDto
-    {
-        public string Token { get; set; }
-        public string RefreshToken { get; set; } //tokenın belli bir süresi olucak.sonrasında yenilencek genelde 20 dk'dır.güvenliği bunla sağlamam önemli
-    }
-}
